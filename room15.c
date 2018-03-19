f1052ff24843b0a42da73c18fec5d4905b3da996
Sean <fujinvsjax@gmail.com>
2018-03-19 16:47:13 -0700

    This is version 2 of the source file

--- room15.c
+++ room15.c
@@ -7,7 +7,13 @@
 
 int main(void)
 {
-
+	printf("Hello! Welcome, gamer! You are now in room 15...");
+	printf("You hear a loud growl. Suddenly you hear loud footsteps. You panick but you try to rely on your 
+	instincts");
+	printf("You are shocked to your eyes to see a massive bear coming out of a cave!");
+	printf("You have a piece of rock laying next to you and a long club laying further away");
+	printf("What do you do?");
+	int userinput;
 
 
 
