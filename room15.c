fbd0a7914a6cc60fa946fe6b92fad681d7f663d8
Sean <fujinvsjax@gmail.com>
2018-03-19 17:02:17 -0700

    This is version 3

--- room15.c
+++ room15.c
@@ -7,13 +7,28 @@
 
 int main(void)
 {
+	int userinput;
 	printf("Hello! Welcome, gamer! You are now in room 15...");
 	printf("You hear a loud growl. Suddenly you hear loud footsteps. You panick but you try to rely on your 
 	instincts");
 	printf("You are shocked to your eyes to see a massive bear coming out of a cave!");
 	printf("You have a piece of rock laying next to you and a long club laying further away");
-	printf("What do you do?");
-	int userinput;
+	printf("What do you do? Please choose from the following options!");
+	printf("Press 99 if you want to quit since video games cause violence");
+	
+	scanf(%d, &userinput);
+	while(userinput!=99)
+	{
+		if(userinput ==1) 
+		{
+			printf("You automatically rely on running out of the room. The room is locked out and there is no other exit besides the 
+				cave that the bear came out of. Since you're panick attack has gotten worse, you make poor judgments and gets 
+				beaten to death by the bear. Now you have lost your legacy!);
+		}
+
+		
+
+
 
 
 
